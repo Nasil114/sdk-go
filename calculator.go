73228dc9b60d06e43345640b9a834225b0d30f31
Nasil114 <120000998+Nasil114@users.noreply.github.com>
2023-11-23 19:24:46 +0530

    Create calculator.go

---
+++ calculator.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// Function to calculate the sum of two numbers
+func sum(a, b int) int {
+	return a + b
+}
+
+func main() {
+	// Example usage
+	result := sum(5, 10)
+	fmt.Println("Result of the sum:", result)
+}
